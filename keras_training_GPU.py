ff3cbf72fbc3ea24f16523d3eabd69f11a3d54e5
balaspritle <78598404+balaspritle@users.noreply.github.com>
2021-03-19 11:22:58 +0530

    Add files via upload

---
+++ keras_training_GPU.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+with tf.device("gpu:0"):
+   print("tf.keras code in this scope will run on GPU")
+
+print("\n\n Step 1: Prepare the dataset to train the model")
+
+(ds_train, ds_test), ds_info = tfds.load(
+    'mnist',
+    split=['train', 'test'],
+    shuffle_files=True,
+    as_supervised=True,
+    with_info=True,
+)
+
+def normalize_img(image, label):
+  """Normalizes images: `uint8` -> `float32`."""
+  return tf.cast(image, tf.float32) / 255., label
+
+ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+
+ds_test = ds_test.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_test = ds_test.batch(128)
+ds_test = ds_test.cache()
+ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+
+print("\n\n Step 2: Create and train the model")
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128,activation='relu'),
+  tf.keras.layers.Dense(10)
+])
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.001),
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+)
+
+model.fit(
+    ds_train,
+    epochs=15,
+    validation_data=ds_test,
+)
+
+
+
